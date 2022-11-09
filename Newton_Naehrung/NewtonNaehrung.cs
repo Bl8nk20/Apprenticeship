97976abc10987e3be963c9058ff08b90090b3b01
Bl8nk20 <94741379+Bl8nk20@users.noreply.github.com>
2022-11-09 08:13:34 +0100

    Add files via upload

---
+++ Newton_Naehrung/NewtonNaehrung.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+namespace NewtonNaehrerung
+{
+    public class Newton
+    {
+}
