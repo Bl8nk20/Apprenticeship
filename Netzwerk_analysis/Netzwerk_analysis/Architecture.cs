b063f54eb3576d93694ebe9650f722d99be84e2d
Bl8nk20 <94741379+Bl8nk20@users.noreply.github.com>
2022-12-15 12:28:19 +0100

    Current Status: 21.15.2022-12:26
    A Program to Calculate the Currents and Voltages of a static / given schematic. 
    
    Tried to code with a better Class Architecture.

---
+++ Netzwerk_analysis/Netzwerk_analysis/Architecture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netzwerk_analysis
+{
+    // 1. Data Gathering -> read File + User_input Main Window
+    // 2. Data Manipulation -> Create Matrix | Create Vector -> Vector/ Array -> Currents in Mesh
+    // 3. Currents in Mesh -> Currents in Junction
+    // 4. Junction Comparision -> bool questioning
+    // 5. Calculate Voltages at each Resistor -> Ohm´s Law
+    // 6. Output -> Window | File | Image in MainWindow
+
+
+    // Interface for Data Gathering -> Reading and Writing File and gathering MainWindow informations
+    public interface IDataProvider<T>
+    {
+        IEnumerable<T> GetData();
+    }
+
+    // Interface for Manipulation Data -> Currents in Junctions
+    public interface IManipulator<T, V>
+    {
+        V Manipulate(T data);
+    }
+
+    // Interface for Data output
+    public interface IWriter<T>
+    {
+        void Write(T data);
+    }
+}
